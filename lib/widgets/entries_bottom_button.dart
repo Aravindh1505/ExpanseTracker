805b058583aad00098c39c7546132f18a4d981d6
asamidur-ford <asamidur@ford.com>
2023-02-15 23:17:10 +0530

    Minor issues fixed

--- lib/widgets/entries_bottom_button.dart
+++ lib/widgets/entries_bottom_button.dart
@@ -13,42 +13,40 @@ class EntriesBottomButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(5.0),
-      child: Expanded(
-        child: Align(
-          alignment: Alignment.bottomCenter,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
-                  foregroundColor: Colors.white,
-                  fixedSize: const Size(150, 30),
-                  textStyle: const TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w500,
-                  ),
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.white,
+                fixedSize: const Size(150, 30),
+                textStyle: const TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w500,
                 ),
-                onPressed: () => cashInAndOut(context, CashType.CASH_IN, book),
-                icon: const Icon(Icons.add),
-                label: const Text('CASH IN'),
               ),
-              ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w500,
-                  ),
+              onPressed: () => cashInAndOut(context, CashType.CASH_IN, book),
+              icon: const Icon(Icons.add),
+              label: const Text('CASH IN'),
+            ),
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w500,
                 ),
-                onPressed: () => cashInAndOut(context, CashType.CASH_OUT, book),
-                icon: const Icon(Icons.remove),
-                label: const Text('CASH OUT'),
-              )
-            ],
-          ),
+              ),
+              onPressed: () => cashInAndOut(context, CashType.CASH_OUT, book),
+              icon: const Icon(Icons.remove),
+              label: const Text('CASH OUT'),
+            )
+          ],
         ),
       ),
     );
